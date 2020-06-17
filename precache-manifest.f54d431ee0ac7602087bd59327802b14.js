b0f081711f9bb841d41dc49e88a638a823af853b
Morton <morton.nari@yahoo.com.au>
2020-06-17 22:30:24 +1000

    Updates

---
+++ precache-manifest.f54d431ee0ac7602087bd59327802b14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54a74e16f47c8ef88aafa4317a8ca9f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ff37a5bb08a4f71867c0",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "119f8689d0f776bf568e",
+    "url": "/contactmanager/static/css/main.f228f44d.chunk.css"
+  },
+  {
+    "revision": "ff37a5bb08a4f71867c0",
+    "url": "/contactmanager/static/js/2.caf91d09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.caf91d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119f8689d0f776bf568e",
+    "url": "/contactmanager/static/js/main.5bbb6874.chunk.js"
+  },
+  {
+    "revision": "31aa565394677d9739a4",
+    "url": "/contactmanager/static/js/runtime-main.ab1f59d6.js"
+  }
+]);
